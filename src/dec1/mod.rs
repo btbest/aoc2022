378231a194fc52b94a94924813118380ca92795f
Benedikt Best <63287233+btbest@users.noreply.github.com>
2022-12-23 16:20:03 +0100

    hooray for finishing day 1 on 23 December :D

--- src/dec1/mod.rs
+++ src/dec1/mod.rs
@@ -1,16 +1,11 @@
-use std::{path::{Path, PathBuf}, fs::File, io::{BufReader, BufRead}};
+use std::{path::Path, fs::read_to_string};
 
 pub fn task1() {
-    let data = load_from_file(Path::new("inputs").join("dec1_task1.txt"));
-    println!("{:?}", data);
-}
-
-fn load_from_file(file_path: PathBuf) {
-    let file = File::open(file_path).expect("file wasn't found.");
-    let reader = BufReader::new(file);
-
-    let numbers: Vec<i64> = reader
-        .lines()
-        .map(|line| line.unwrap().parse::<i64>().unwrap_or_else())
-        .collect();
+    let data = read_to_string(Path::new("inputs").join("dec1_task1.txt")).unwrap();
+    let highest_elf_value: i32 = data.split("\r\n\r\n").map(
+        |single_elf_lines| single_elf_lines.split("\r\n").map(
+            |line| line.parse::<i32>().unwrap_or_default()
+        ).sum()
+    ).max().unwrap();
+    println!("Day 1, task 1. Highest calorie load carried by any elf: {:?}",highest_elf_value)
 }
