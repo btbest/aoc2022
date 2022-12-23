87bf96fa1cde8f0f68220297c3316394833d325c
Benedikt Best <63287233+btbest@users.noreply.github.com>
2022-12-23 15:33:46 +0100

    not quite

--- src/dec1/mod.rs
+++ src/dec1/mod.rs
@@ -1,3 +1,16 @@
+use std::{path::{Path, PathBuf}, fs::File, io::{BufReader, BufRead}};
+
 pub fn task1() {
-    print!("solve task1 of dec1");
+    let data = load_from_file(Path::new("inputs").join("dec1_task1.txt"));
+    println!("{:?}", data);
+}
+
+fn load_from_file(file_path: PathBuf) {
+    let file = File::open(file_path).expect("file wasn't found.");
+    let reader = BufReader::new(file);
+
+    let numbers: Vec<i64> = reader
+        .lines()
+        .map(|line| line.unwrap().parse::<i64>().unwrap_or_else())
+        .collect();
 }
